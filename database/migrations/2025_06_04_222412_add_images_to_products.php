3311cb882fab9a95de69dc486c577cbb22523519
Yariangel Aray <gabrielamunar4@gmail.com>
2025-06-04 17:56:28 -0500

    laravel practicas

---
+++ database/migrations/2025_06_04_222412_add_images_to_products.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('img_id')->nullable();
+            $table->foreign('img_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_img_id_foreign'); // (tabla)_(campo de la tabla)_foreign
+            $table->dropColumn('img_id');
+        });
+    }
+};
